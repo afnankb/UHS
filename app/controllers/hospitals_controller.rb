863324a0550786a82812f0396e56e2ff2618d165
bedouralshuaibi <b.f.alshuaibi@gmail.com>
2020-04-04 15:20:56 +0300

    show page and index page

---
+++ app/controllers/hospitals_controller.rb
@@ -0,0 +1,11 @@
+class HospitalsController < ApplicationController
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+      resources :hospitals
   get 'pages/home'
-  devise_for :users
+  # get 'hospitals/:id', to: 'hospitals#show', as: 'hospitals'
+  
   root to: "pages#home"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
