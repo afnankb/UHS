887a658360c4c2433c3457169cda05ece13ebbef
afnankb <afnankbasudan@gmail.com>
2020-04-05 13:24:43 +0300

    add hospitals in need page

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,11 +1,22 @@
 class HospitalsController < ApplicationController
 
     def index
-        @users = User.all
+         if current_user.id > 1
+            redirect_to supplies_path
+         else 
+          @users = User.all
+         end 
     end
 
     def show
         @user = User.find(params[:id])
+        if current_user.id > 1
+        if @user != current_user
+          flash[:notice] = 'Not allowed!'
+          redirect_to supplies_path
+        end
+    end 
     end
+
 
 end
